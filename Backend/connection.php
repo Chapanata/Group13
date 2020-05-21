cdb983823903126d017e09a9708a9bad2dae96ec
Jorde <jorde@jordes-mbp.lan>
2020-05-21 13:15:30 -0400

    Moved utils into connection.php

--- Backend/connection.php
+++ Backend/connection.php
@@ -5,4 +5,16 @@ function dbConnection()
 	return (new PDO('mysql:host=localhost;dbname=contact_deluxe','cmd_user','Kjs18E&9'));
 }
 
+function error($errorMsg)
+{
+    $error = array('Error' => $errorMsg);
+    echo(json_encode($error));
+}
+
+function closeConnectionAndDie($conn)
+{
+    $conn->close();
+    die();
+}
+
 ?>
